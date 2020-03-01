79b23534f7869cbcbbd58b03900dc3c2705676b4
jaiprakash2612 <jay.patidar2612@gmail.com>
2020-03-01 20:43:46 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import {Switch,Route,Link} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 
-function App() {
+const Hatspage = (props) => {
+  console.log(props);
   return (
-    <HomePage/>
+    <div>
+      <h1>Hats</h1>
+    </div>
+  )
+}
+const TopicsLists = (props) => {
+  console.log(props);
+  const topicId = 7
+  return (
+    <div>
+      <Link to={'/topics/'+topicId }> To Topic 7</Link>
+      <Link to={`${props.match.url}/9`}> To Topic 9</Link>
+      <h1>TopicsLists</h1>
+    </div>
+  )
+}
+const Topic  = (props) => {
+  console.log(props);
+return  (
+  <div>
+    <h1>Topic Details : {props.match.params.topicId}</h1>
+  </div>
+)
+}
+function App() {
+  return ( 
+    <div>
+      <Switch>
+        {/* <Route  exact path='/' component={HomePage}/>
+        <Route exact path='/topics' component={TopicsLists}/>
+        <Route path='/topics/:topicId' component={Topic}/> */}
+        <Route  exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={Hatspage}/>
+        </Switch>
+     
+    </div>
+    
   );
 }
 
